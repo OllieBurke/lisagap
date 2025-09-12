86632453c3da5c25a1713854013e453affbfad07
Ollie Burke <ollie.burke@glasgow.ac.uk>
2025-09-12 23:19:49 +0100

    automatic version changing

--- lisagap/__init__.py
+++ lisagap/__init__.py
@@ -1,5 +1,17 @@
 from .gap_mask_generator import GapMaskGenerator
 
-__version__ = "0.2.0"
-__author__ = "Ollie Burke"
+from importlib.metadata import version
+
+__version__ = version("lisa-gap")
+__author__ = "Ollie Burke -- ollie.burke@glasgow.ac.uk"
 __all__ = ["GapMaskGenerator"]
+
+__citation__ = (
+    "\n@software{lisagap,"
+    "\nauthor = {Burke, Ollie and Castelli, Eleonora},"
+    "\ntitle = {lisa-gap: A tool for simulating data gaps in LISA time series},"
+    "\nurl = {https://github.com/ollieburke/lisa-gap},"
+    f"\nversion = {{{__version__}}},"
+    "\nyear = {2025}"
+    "}\n"
+)
